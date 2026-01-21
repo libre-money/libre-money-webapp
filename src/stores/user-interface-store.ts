cdb1d824bf78652bd5b5fdddce44a7dbaabe4ce1
Sayem Shafayet <ishafayet@gmail.com>
2026-01-21 19:52:15 +0600

    #65 Enhance MainLayout with collapsible Entities section
    - Added a clickable header for the Entities section in MainLayout, allowing users to expand and collapse the list of entities.
    - Implemented a transition effect for the expanded view of entities.
    - Updated user interface store to manage the expanded state of the Entities section, including local storage handling for persistence.

--- src/stores/user-interface-store.ts
+++ src/stores/user-interface-store.ts
@@ -8,6 +8,7 @@ const LOCAL_STORAGE_KEY__ACCOUNTING_EXPANDED = "--lm-ui--accounting-expanded";
 const LOCAL_STORAGE_KEY__REPORTS_EXPANDED = "--lm-ui--reports-expanded";
 const LOCAL_STORAGE_KEY__ADVANCED_EXPANDED = "--lm-ui--advanced-expanded";
 const LOCAL_STORAGE_KEY__PAYABLES_RECEIVABLES_EXPANDED = "--lm-ui--payables-receivables-expanded";
+const LOCAL_STORAGE_KEY__ENTITIES_EXPANDED = "--lm-ui--entities-expanded";
 
 function deserializeBoolean(localStorageKey: string, defaultValue = false) {
   const value = localStorage.getItem(localStorageKey);
@@ -22,9 +23,10 @@ function deserializeBoolean(localStorageKey: string, defaultValue = false) {
 export const useUserInterfaceStore = defineStore("userInterfaceStore", {
   state: () => ({
     accountingExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__ACCOUNTING_EXPANDED, false),
-    reportsExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__REPORTS_EXPANDED, false),
+    reportsExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__REPORTS_EXPANDED, true),
     advancedExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__ADVANCED_EXPANDED, false),
     payablesReceivablesExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__PAYABLES_RECEIVABLES_EXPANDED, true),
+    entitiesExpanded: deserializeBoolean(LOCAL_STORAGE_KEY__ENTITIES_EXPANDED, true),
   }),
 
   getters: {},
@@ -45,6 +47,10 @@ export const useUserInterfaceStore = defineStore("userInterfaceStore", {
     setPayablesReceivablesExpanded(value: boolean) {
       serializeValue(LOCAL_STORAGE_KEY__PAYABLES_RECEIVABLES_EXPANDED, value);
       this.payablesReceivablesExpanded = value;
+    },
+    setEntitiesExpanded(value: boolean) {
+      serializeValue(LOCAL_STORAGE_KEY__ENTITIES_EXPANDED, value);
+      this.entitiesExpanded = value;
     },
   },
 });
